12d89335567de7f818bbae72b3b356babe3e8a8e
Francolove <37364514+Francolove@users.noreply.github.com>
2018-03-15 13:17:57 +0100

    add new file

--- induced_line_speed.m
+++ induced_line_speed.m
@@ -1,14 +1,14 @@
 function V = induced_line_speed(line,gamma,point)
 % Line = [ fine_vortice  inizio_vortice]
-% calcola la velocità indotta da un anello vorticoso avente intensità gamma
+% calcola la velocità indotta da una linea vorticosa avente intensità gamma
 % nel punto / nei punti descritti da point
 %
-% line 2,1,3
-% gamma = 1
+% line 1,2,3 o matrice colonna
+% gamma = 1 o vettore colonna
 % point 1,1,3 o n,n,3
 
-    r1 = point-line(1,2,:);
-    r2 = point-line(1,1,:); 
+    r1 = point-line(:,2,:);
+    r2 = point-line(:,1,:); 
 
     % if sqrt(sum(r1.^2,3)) == 0 || sqrt(sum(r2.^2,3)) == 0
     % V = zeros(1,1,3);
@@ -19,7 +19,7 @@ function V = induced_line_speed(line,gamma,point)
     num =(sqrt(sum(r1.^2,3))+sqrt(sum(r2.^2,3))).*cross(r1,r2,3);
     den = sqrt(sum(r1.^2,3)).*sqrt(sum(r2.^2,3)).*...
                     (sqrt(sum(r1.^2,3)).*sqrt(sum(r2.^2,3))+dot(r1,r2,3));
-    V = gamma/(4*pi)*num./den;
+    V = gamma/(4*pi).*num./den;
     if ~isempty(riga1)
         V(riga1,colonna1,:) = zeros(size(riga1,2),size(colonna1,2),3);
     end
